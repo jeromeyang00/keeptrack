8f658797ea85c66f17f2ca018ec27f365ef3c46c
Eduard Roque <125721249+EADRoque@users.noreply.github.com>
2023-11-30 10:14:51 +0800

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+import ProjectsPage from './projects/ProjectsPage';
+
+function App() {
+  return (
+    <div className='container'>
+      <ProjectsPage/>
+    </div>
+  );
+}
+
+export default App;
